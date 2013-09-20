4dafada1cb9d58a98bf863d30e797d875f76e93c
caiiiycuk <caiiiycuk@gmail.com>
2013-09-20 09:06:18 +0700

    Add get for seq

--- src/main/scala/async4s/dsl/Async4sDSL.scala
+++ src/main/scala/async4s/dsl/Async4sDSL.scala
@@ -19,22 +19,22 @@ object Async4sDSL {
     new RequestUrl(url, STRING)
 
   private def _get[T](r: RequestUrl[T])(implicit client: AsyncHttpClient, ec: ExecutionContext) =
-    future { client.prepareGet(r.url).execute.get }
+    future { r.r2T(client.prepareGet(r.url).execute.get) }
 
   def async_get[T](r: RequestUrl[T])(implicit client: AsyncHttpClient, ec: ExecutionContext): Future[T] =
-    for (rr <- _get(r)) yield r.r2T(rr)
+    for (rr <- _get(r)) yield rr
 
   def async_get[T1, T2](r1: RequestUrl[T1], r2: RequestUrl[T2])(implicit client: AsyncHttpClient, ec: ExecutionContext): Future[(T1, T2)] =
-    for (rr1 <- _get(r1); rr2 <- _get(r2)) yield (r1.r2T(rr1), r2.r2T(rr2))
+    for (rr1 <- _get(r1); rr2 <- _get(r2)) yield (rr1, rr2)
 
   def async_get[T1, T2, T3](r1: RequestUrl[T1], r2: RequestUrl[T2], r3: RequestUrl[T3])(implicit client: AsyncHttpClient, ec: ExecutionContext): Future[(T1, T2, T3)] =
-    for (rr1 <- _get(r1); rr2 <- _get(r2); rr3 <- _get(r3)) yield (r1.r2T(rr1), r2.r2T(rr2), r3.r2T(rr3))
+    for (rr1 <- _get(r1); rr2 <- _get(r2); rr3 <- _get(r3)) yield (rr1, rr2, rr3)
 
   def async_get[T1, T2, T3, T4](r1: RequestUrl[T1], r2: RequestUrl[T2], r3: RequestUrl[T3], r4: RequestUrl[T4])(implicit client: AsyncHttpClient, ec: ExecutionContext): Future[(T1, T2, T3, T4)] =
-    for (rr1 <- _get(r1); rr2 <- _get(r2); rr3 <- _get(r3); rr4 <- _get(r4)) yield (r1.r2T(rr1), r2.r2T(rr2), r3.r2T(rr3), r4.r2T(rr4))
-    
+    for (rr1 <- _get(r1); rr2 <- _get(r2); rr3 <- _get(r3); rr4 <- _get(r4)) yield (rr1, rr2, rr3, rr4)
+
   def async_get[T1, T2, T3, T4, T5](r1: RequestUrl[T1], r2: RequestUrl[T2], r3: RequestUrl[T3], r4: RequestUrl[T4], r5: RequestUrl[T5])(implicit client: AsyncHttpClient, ec: ExecutionContext): Future[(T1, T2, T3, T4, T5)] =
-    for (rr1 <- _get(r1); rr2 <- _get(r2); rr3 <- _get(r3); rr4 <- _get(r4); rr5 <- _get(r5)) yield (r1.r2T(rr1), r2.r2T(rr2), r3.r2T(rr3), r4.r2T(rr4), r5.r2T(rr5))    
+    for (rr1 <- _get(r1); rr2 <- _get(r2); rr3 <- _get(r3); rr4 <- _get(r4); rr5 <- _get(r5)) yield (rr1, rr2, rr3, rr4, rr5)
 
   def get[T](r: RequestUrl[T])(implicit client: AsyncHttpClient, ec: ExecutionContext): T =
     await(async_get(r))
@@ -47,9 +47,15 @@ object Async4sDSL {
 
   def get[T1, T2, T3, T4](r1: RequestUrl[T1], r2: RequestUrl[T2], r3: RequestUrl[T3], r4: RequestUrl[T4])(implicit client: AsyncHttpClient, ec: ExecutionContext): (T1, T2, T3, T4) =
     await(async_get(r1, r2, r3, r4))
-    
+
   def get[T1, T2, T3, T4, T5](r1: RequestUrl[T1], r2: RequestUrl[T2], r3: RequestUrl[T3], r4: RequestUrl[T4], r5: RequestUrl[T5])(implicit client: AsyncHttpClient, ec: ExecutionContext): (T1, T2, T3, T4, T5) =
-    await(async_get(r1, r2, r3, r4, r5))    
+    await(async_get(r1, r2, r3, r4, r5))
+
+  def async_get[T](rs: Seq[RequestUrl[T]])(implicit client: AsyncHttpClient, ec: ExecutionContext): Future[Seq[T]] =
+    Future.sequence(for (r <- rs) yield _get(r))
+
+  def get[T](r: Seq[RequestUrl[T]])(implicit client: AsyncHttpClient, ec: ExecutionContext): Seq[T] =
+    await(async_get(r))
 
   private def await[T](f: Future[T])(implicit ec: ExecutionContext): T = {
     Await.result(f, Duration.Inf)
--- src/test/scala/async4s/test/GET.scala
+++ src/test/scala/async4s/test/GET.scala
@@ -4,6 +4,7 @@ import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 import async4s.dsl.Async4sDSL
 import async4s.impl.Async4sTestClient
+import async4s.request.RequestUrl
 
 class GET extends FlatSpec with ShouldMatchers {
 
@@ -41,30 +42,45 @@ class GET extends FlatSpec with ShouldMatchers {
 
   it should "GET four urls" in {
     val (google, bing, yahoo, duckduckgo) =
-      get("http://google.com", 
-          "http://www.bing.com", 
-          "http://www.yahoo.com",
-          "https://duckduckgo.com/")
+      get("http://google.com",
+        "http://www.bing.com",
+        "http://www.yahoo.com",
+        "https://duckduckgo.com/")
 
     google should include("google")
     bing should include("bing")
     yahoo should include("yahoo")
     duckduckgo should include("duckduckgo")
   }
-  
+
   it should "GET five urls" in {
     val (google, bing, yahoo, duckduckgo, yandex) =
-      get("http://google.com", 
-          "http://www.bing.com", 
-          "http://www.yahoo.com",
-          "https://duckduckgo.com/",
-          "http://www.yandex.ru")
+      get("http://google.com",
+        "http://www.bing.com",
+        "http://www.yahoo.com",
+        "https://duckduckgo.com/",
+        "http://www.yandex.ru")
 
     google should include("google")
     bing should include("bing")
     yahoo should include("yahoo")
     duckduckgo should include("duckduckgo")
     yandex should include("yandex")
+  }
+
+  it should "GET urls from seq" in {
+    val responses = get(Seq[RequestUrl[String]](
+      "http://google.com",
+      "http://www.bing.com",
+      "http://www.yahoo.com",
+      "https://duckduckgo.com/",
+      "http://www.yandex.ru"))
+
+    responses(0) should include("google")
+    responses(1) should include("bing")
+    responses(2) should include("yahoo")
+    responses(3) should include("duckduckgo")
+    responses(4) should include("yandex")
   }
 
 }
