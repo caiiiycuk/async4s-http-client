a8b2e381b936fe9aae501712783f2562c46e310a
caiiiycuk <caiiiycuk@gmail.com>
2013-09-27 14:37:28 +0700

    Add test for Any2Param

--- src/test/scala/com/github/caiiiycuk/async4s/test/Params.scala
+++ src/test/scala/com/github/caiiiycuk/async4s/test/Params.scala
@@ -5,6 +5,8 @@ import org.scalatest.matchers.ShouldMatchers
 import com.github.caiiiycuk.async4s.dsl.Async4sDSL
 import com.github.caiiiycuk.async4s.impl.Async4sTestClient
 import com.github.caiiiycuk.async4s.request.RequestUrl
+import com.github.caiiiycuk.async4s.request.RequestUrl
+import com.github.caiiiycuk.async4s.util.Any2Param
 
 class Params extends FlatSpec with ShouldMatchers {
 
@@ -13,9 +15,21 @@ class Params extends FlatSpec with ShouldMatchers {
 
   behavior of "Params dsl"
 
-  it should "POST single url as string" in {
-    val content = post("http://google.com" ~ ("" -> ""))
-    content should include("Method Not Allowed")
+  it should "convert pairs to params" in {
+    val x: RequestUrl[String] =
+      "http://url" ~ ("user" -> "caiiiycuk") ~ ("token" -> "abcdedwqsdq") ~ ("array" -> Seq(1, 2, 3))
+    
+    val first = x.params(0)
+    val second = x.params(1)
+    val third = x.params(2)
+      
+    first.key should equal("user")
+    second.key should equal("token")
+    third.key should equal("array")
+    
+    Any2Param.any2Param(first.value) should equal(Seq("caiiiycuk"))
+    Any2Param.any2Param(second.value) should equal(Seq("abcdedwqsdq"))
+    Any2Param.any2Param(third.value) should equal(Seq("1", "2", "3"))
   }
-
+  
 }
